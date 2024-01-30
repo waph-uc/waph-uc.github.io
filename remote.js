7b6c8b4c88c3190e2eb0535af173933e8cd1f138
phungph-uc <154654461+phungph-uc@users.noreply.github.com>
2024-01-29 21:55:55 -0500

    Update remote.js

--- remote.js
+++ remote.js
@@ -1,1 +1,2 @@
-document.write('Hello, I come from https://waph-uc.github.io/, but I can read your cookie: '+document.cookie);
+document.write('Hello, I come from https://waph-uc.github.io/, but I can get your credentials at ' + 
+               location.origin + ', e.g., your cookie: '+ document.cookie);
